fbaf1fa41aa0dbf9394d9889a9ddf0006065fd0c
Baoming Bai <tskyming@163.com>
2015-04-14 14:45:41 +0800

    init

---
+++ BeaconActiveSDKData-ObjC/SensoroSDKActiveData-ObjC/SensoroSDKActiveData-ObjC/SENLocationManager.m
@@ -0,0 +1,151 @@
+//
+//  SENLocationManager.m
+//  BeaconActive-ObjC
+//
+//  Created by skyming on 15/4/10.
+//  Copyright (c) 2015年 Sensoro. All rights reserved.
+//
+
+#import "SENLocationManager.h"
+#import <UIKit/UIKit.h>
+#import <SensoroBeaconKit/SensoroBeaconKit.h>
+
+@interface SENLocationManager ()<SBKBeaconManagerDelegate>
+
+@end
+
+@implementation SENLocationManager
+
++ (instancetype)sharedInstance
+{
+    static dispatch_once_t once;
+    static id sharedInstance;
+    dispatch_once(&once, ^{
+        sharedInstance = [[self alloc] init];
+    });
+    return sharedInstance;
+}
+
+- (instancetype)init{
+    
+    if(self = [super init])
+    {
+        _locationManager = [SBKBeaconManager sharedInstance];
+
+        if ([[[UIDevice currentDevice] systemVersion] floatValue] >= 8.0)
+        {
+            //获取授权
+            [_locationManager requestAlwaysAuthorization];
+        }
+        //设定代理
+        _locationManager.delegate = self;
+        //生成监测用的区域ID
+        _monitorRegion = [SBKBeaconID beaconIDWithProximityUUID:[[NSUUID alloc]initWithUUIDString:@"46D06053-9FAD-483B-B704-E576735CE1A3"] major:0x23A2 minor:0x854D];
+        
+    }
+    return self;
+}
+
+
+- (void)startMonitor:(BOOL)relaunch{
+    
+    if (!relaunch) {
+        [_locationManager startRangingBeaconsWithID:_monitorRegion wakeUpApplication:YES];
+        NSLog(@"Start monitor region!");
+    }else{
+        NSLog(@"During the relauch app, don't restart monitor region!");
+        [_locationManager startRangingBeaconsWithID:_monitorRegion wakeUpApplication:NO];
+
+    }
+    _started = true;
+
+}
+
+- (void)stopMonitor{
+    [_locationManager stopRangingBeaconsWithID:_monitorRegion];
+    _started = false;
+    NSLog(@"Stop monitor region!");
+}
+
+- (void)sendNotification:(NSString *)notify{
+    UILocalNotification *notification = [[UILocalNotification alloc]init];
+    notification.alertBody = notify;
+    notification.soundName = UILocalNotificationDefaultSoundName;
+    
+    [[UIApplication sharedApplication] scheduleLocalNotification:notification];
+}
+
+
+- (void)beaconManager:(SBKBeaconManager *)beaconManager didRangeNewBeacon:(SBKBeacon *)beacon{
+   
+    NSDate *now = [NSDate date];
+    NSDateFormatter *formatter = [[NSDateFormatter alloc]init];
+    formatter.dateFormat = @"YYYY/MM/dd HH:mm:ss";
+    
+    NSLog(@"Enter: %@",now);
+}
+
+- (void)beaconManager:(SBKBeaconManager *)beaconManager beaconDidGone:(SBKBeacon *)beacon{
+    NSDate *now = [NSDate date];
+    NSDateFormatter *formatter = [[NSDateFormatter alloc]init];
+    formatter.dateFormat = @"YYYY/MM/dd HH:mm:ss";
+    
+    NSLog(@"Exit: %@",now);
+}
+
+- (void)beaconManager:(SBKBeaconManager *)beaconManager didDetermineState:(SBKRegionState)state forRegion:(SBKBeaconID *)region{
+  
+    NSDate *now = [NSDate date];
+    NSDateFormatter *formatter = [[NSDateFormatter alloc]init];
+    formatter.dateFormat = @"YYYY/MM/dd HH:mm:ss";
+    
+    switch (state) {
+        case SBKRegionStateEnter:
+        {
+            NSString *alertbody =[NSString stringWithFormat:@"Enter region at %@",[formatter stringFromDate:now]];
+            [self sendNotification:alertbody];
+            NSLog(@"%@",alertbody);
+        }
+            break;
+            
+        case SBKRegionStateLeave:
+        {
+            NSString *alertbody =[NSString stringWithFormat:@"Exit region at %@",[formatter stringFromDate:now]];
+            [self sendNotification:alertbody];
+            NSLog(@"%@",alertbody);
+        }
+            break;
+            
+        case SBKRegionStateUnknown:
+            NSLog(@"This region state was unknown!");
+            break;
+    }
+
+    if ([UIApplication sharedApplication].applicationState != UIApplicationStateActive) {
+        __block UIBackgroundTaskIdentifier bgTask = [[UIApplication sharedApplication] beginBackgroundTaskWithExpirationHandler:^{
+            // Clean up any unfinished task business by marking where you
+            // stopped or ending the task outright.
+            [self sendNotification:@"后台任务结束"];
+            [[UIApplication sharedApplication] endBackgroundTask:bgTask];
+            bgTask = UIBackgroundTaskInvalid;
+        }];
+    }
+
+}
+
+- (void)beaconManager:(SBKBeaconManager *)beaconManager scanDidFinishWithBeacons:(NSArray *)beacons{
+    
+    NSLog(@"应用已被激活 %d",(int)beacons.count);
+    
+    SBKBeacon *beacon = nil;
+    if (beacons.count > 0) {
+        beacon = beacons[0];
+        NSLog(@"Beacon: %@ %@", beacon,beacon.beaconID.debugDescription);
+        NSString *alertbody =[NSString stringWithFormat:@"Beacon %@ %@ %@ %@ %@ %@ %@",beacon.beaconID.proximityUUID.UUIDString, beacon.beaconID.major,beacon.beaconID.minor,beacon.batteryLevel,beacon.temperature,beacon.light,beacon.accelerometerCount];
+        [self sendNotification:alertbody];
+    }
+    
+}
+
+
+@end
---
+++ README.md
@@ -0,0 +1,70 @@
+# FindBeaconWhenReActive-iOS
+用于演示并测试App被Kill后激活，重启后激活，修改语言后激活等表现是否可以拿到Beacon的UMM、传感器数据等。
+
+##目录内容
+1. BeaconSDKActiveData-Swift，使用苹果标准的API实现的后台唤醒后是否可以获取Beacon数据。
+2. SensoroSDKActiveData-iOS，使用Sensoro SDK实现的后台唤醒后是否可以获取Beacon数据。
+
+##测试环境
+
+* 测试机型：iPhone 6
+* 系统版本：iOS 8.3
+* iBeacon配置：
+ 	* UUID : 46D06053-9FAD-483B-B704-E576735CE1A3
+ 	* Major & Minor : 任意
+	* 也可以使用配置工具扫描App中的二维码进行配置。
+
+##测试: 
+
+####Kill掉App后，进入离开区域，测试Beacon设备数据。
+
+UMM ：46D06053-9FAD-483B-B704-E576735CE1A3|0x23A2|0x854D
+
+型号 | 电量 | 温度 | 亮度| 加速度
+--- | ---- | --- | ---- | ---- 
+A0  | 0.92 |null | null | null
+A0  | 0.93 |null | null | null
+B0  | 0.71 | 18  | 13.95| 0
+B0  | 0.71 | 18  | 18.08| 3
+C0  | 0.99 | 23  | null | 10 
+C0  | 0.99 | 21  | null | 12 
+
+####设定语言后的重启。
+
+与Kill掉App效果基本相同。
+
+####重启后，进入离开区域，测定Beacon设备数据。
+
+UMM ：46D06053-9FAD-483B-B704-E576735CE1A3|0x23A2|0x854D
+
+型号 | 电量 | 温度 | 亮度| 加速度
+--- | ---- | --- | ---- | ---- 
+A0  | 0.94 |null |null|null
+A0  | 0.87 |null |null|null
+B0  | 0.55 | 18  | 37.62| 0
+B0  | 0.55 | 18  | 37.44| 0
+C0  | 0.99 | 23  | null | 10 
+C0  | 0.99 | 20  | null | 17 
+
+####注意事项
+* 唤醒后获取Beacon数据测试代码是基于[Beacon-Active-iOS](https://github.com/Sensoro/Beacon-Active-iOS)
+
+* 唤醒后在 startMonitor: 内添加如下代码
+
+```Object-C
+ [_locationManager startRangingBeaconsWithID:_monitorRegion wakeUpApplication:NO];
+
+```
+* 在 beaconManager： didDetermineState：forRegion：添加
+
+```Object-C
+    if ([UIApplication sharedApplication].applicationState != UIApplicationStateActive) {
+        __block UIBackgroundTaskIdentifier bgTask = [[UIApplication sharedApplication] beginBackgroundTaskWithExpirationHandler:^{
+            // Clean up any unfinished task business by marking where you
+            // stopped or ending the task outright.
+            [self sendNotification:@"后台任务结束"];
+            [[UIApplication sharedApplication] endBackgroundTask:bgTask];
+            bgTask = UIBackgroundTaskInvalid;
+        }];
+    }
+```
